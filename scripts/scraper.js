2c9505e60f906a9070d8a7d3deca7ce7005b487d
unknown <tacmb@ELMV0153.int.mcmaster.com>
2017-09-14 08:25:36 -0400

    more typos

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -11,8 +11,8 @@ var getNews = function(url, call) {
         var title = $(this).children(".NewsCard__title--37vMp").text();
         var summary = $(this).children(".NewsCard__content--1VLID").text();
         if (title !== "" && summary !== ""){
-          var cleanTitle = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-          var cleanSummary = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+          var cleanTitle = title.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+          var cleanSummary = summary.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
           articles[i] = [cleanTitle]; 
           articles[i].push(cleanSummary);
         }
