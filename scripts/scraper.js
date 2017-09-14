3672ae53b25c0b0175a955986b3c131a80afcba3
unknown <tacmb@ELMV0153.int.mcmaster.com>
2017-09-14 08:20:52 -0400

    updated variables for handling of articles

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -10,7 +10,7 @@ var getNews = function(url, call) {
       $('.NewsCard__bodyContainer--1h9Eb').each(function(i, element){
         var title = $(this).children(".NewsCard__title--37vMp").text();
         var summary = $(this).children(".NewsCard__content--1VLID").text();
-        if (head !== "" && sum !== ""){
+        if (title !== "" && summary !== ""){
           var cleanTitle = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
           var cleanSummary = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
           articles[i] = [cleanTitle]; 
